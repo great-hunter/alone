7846ba675d6bf59739558b7e637a716c11b7953d
great-hunter <greathunter@mail.ru>
2021-07-01 21:53:06 +0300

    Сделал урок 06

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Привет мир");
+'use strict';
+function games() {
+  let secret = Math.random()*100;
+  secret = Math.round(secret);
+  function play() {
+    let question = prompt('Угадай число от 1 до 100');
+    if(question === null) {
+      alert("Игра окончена");
+      return;
+    }
+    question = +question;
+    if(isNaN(question)) {
+      alert("Введи число!");
+      play();
+    } else if(question > secret) {
+      alert("Загаданное число меньше");
+      play();
+    } else if(question < secret) {
+      alert("Загаданное число больше");
+      play();
+    } else if(question === secret) {
+      alert("Поздравляю, Вы угадали!!!");
+      return;
+    }
+  }
+  play();
+}
+
+games();
